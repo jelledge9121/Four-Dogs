7bc937e73a8fbfea905811ecffb261b38082b067
jelledge9121 <joeyelledge@yahoo.com>
2026-04-25 01:05:49 -0400

    Update FourDogsLogo.tsx

--- components/FourDogsLogo.tsx
+++ components/FourDogsLogo.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image';
-
 export default function FourDogsLogo() {
-  return (
-    <div className="fd-logo" aria-label="Four Dogs Entertainment">
-      <Image
-        src="/four-dogs-logo.png"
-        alt="Four Dogs Entertainment"
-        width={120}
-        height={120}
-        priority
-      />
-    </div>
-  );
+return (
+<div className="fd-logo" aria-label="Four Dogs Entertainment">
+<img
+src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAAEsCAYAAAB5fY51AAEAAElEQVR42u
+alt="Four Dogs Entertainment"
+width={120}
+height={120}
+style={{ objectFit: 'contain' }}
+/>
+</div>
+);
 }
